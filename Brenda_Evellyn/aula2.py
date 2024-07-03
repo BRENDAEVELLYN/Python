33a0112cc98a8bd97435f7e6939d64c0d0ce0d9f
Brenda <evellynluke5@gmail.com>
2024-07-03 11:42:30 -0300

    first commit

---
+++ Brenda_Evellyn/aula2.py
@@ -0,0 +1,8 @@
+ 
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso: "))
+altura= float(input("Digite sua altura: "))
+
+print(f'Meu nome é {nome}, \ntenho{idade} de idade, \nmeu peso é {peso}, tenho{altura}de altura')
+print(type(idade))
---
+++ Brenda_Evellyn/aula3.py
@@ -0,0 +1,9 @@
+
+#Declaração de variável 
+nome='Brenda'
+
+#Convertendo o tipo
+valor = 15
+valor = str(valor)
+
+#Restringir os dados
